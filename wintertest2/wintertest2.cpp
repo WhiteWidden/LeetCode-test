f6c68f0829292d85413dd12665a03ef78cf490ef
WhiteWidden <152471194+WhiteWidden@users.noreply.github.com>
2024-01-24 11:09:21 +0800

    Add files via upload
    面试题 01.07.旋转矩阵

---
+++ wintertest2/wintertest2.cpp
@@ -0,0 +1,67 @@
+﻿// wintertest2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;//实现矩阵顺时针旋转四分之一
+void SWAP(int* a, int* b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+int main()
+{
+    vector<vector<int>> test;
+    int n;
+    cin >> n;
+    test.resize(n);
+    int flag = 1;
+    for (int i = 0; i < test.size(); i++)
+    {
+        test[i].resize(n);
+    }
+    for (int i = 0; i < test.size(); i++)
+    {
+        for (int j = 0; j < test[i].size(); j++)
+        {
+            test[i][j] = flag;
+            cout << test[i][j] << " ";
+            flag++;
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < test.size(); i++)//主对角线翻转
+    {
+        for (int j = i+1; j < test[i].size(); j++)
+        {
+            SWAP(&test[i][j], &test[j][i]);
+        }
+    }
+    for (int i = 0; i < test.size(); i++)//纵轴镜面对称
+    {
+        for (int j = 0; j < test.size()/2; j++)
+        {
+            swap(test[i][j], test[i][test.size()-1 - j]);
+        }
+    }
+    for (int i = 0; i < test.size(); i++)
+    {
+        for (int j = 0; j < test[i].size(); j++)
+        {
+            cout << test[i][j] << " ";   
+        }
+        cout << endl;
+    }
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
