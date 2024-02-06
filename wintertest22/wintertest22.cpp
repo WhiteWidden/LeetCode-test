077e0d6ce38e76eb469ec86779757bc2e2960c6e
WhiteWidden <152471194+WhiteWidden@users.noreply.github.com>
2024-02-07 02:25:22 +0800

    Add files via upload
    153.寻找旋转排序数组中的最小值（二分解法）

---
+++ wintertest22/wintertest22.cpp
@@ -0,0 +1,74 @@
+﻿// wintertest22.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/*已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+
+你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+
+作者：LeetCode
+链接：https://leetcode.cn/leetbook/read/array-and-string/c3ki5/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;//寻找旋转排序数组中的最小值
+void numsinput(vector<int>& nums)
+{
+    int n;
+    cout << "input n:";
+    cin >> n;
+    nums.resize(n);
+    cout << "input elements:";
+    for (auto a = nums.begin(); a != nums.end(); ++a)
+    {
+        cin >> *a;
+    }
+}
+int findMin(vector<int>& nums)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    if (nums[left] < nums[right])
+    {
+        return nums[left];
+    }
+    while (left < right)
+    {
+        int mid = (left + right) / 2;
+        if (nums[mid] < nums[right])
+        {
+            right = mid;
+            //cout << "right=" << right << endl;
+        }
+        else if (nums[mid] > nums[right])
+        {
+            left = mid + 1;
+            //cout << "left=" << left << endl;
+        }
+    }
+    return nums[left];
+}
+int main()
+{
+    vector<int> nums;
+    numsinput(nums);
+    int min = findMin(nums);
+    cout << min;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
